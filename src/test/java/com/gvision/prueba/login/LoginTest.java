b32c9ac3463a53a5d4d2287b09e7f58de9702991
Gerlin Orlando Torres Saavedra <gerlin.orlando@integ.ro>
2017-09-12 15:33:55 -0500

    first commit

---
+++ src/test/java/com/gvision/prueba/login/LoginTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.gvision.prueba.login;
+
+import org.junit.Test;
+
+/**
+ * @author ldgaribello
+ *
+ */
+public class LoginTest {
+	@Test
+	public void testSimple() throws Exception {
+		System.out.println("Hola");
+	}
+}
